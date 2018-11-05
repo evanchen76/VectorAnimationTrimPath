d1a5d536a870da3a6e0497c3b8dc36cddb9d3b8c
Evan <pifan76@gmail.com>
2018-11-05 09:06:45 +0800

    Initial commit

---
+++ app/src/main/java/evan/chen/tutorial/vectoranimationtrimpath/MainActivity.kt
@@ -0,0 +1,19 @@
+package evan.chen.tutorial.vectoranimationtrimpath
+
+import android.graphics.drawable.Animatable
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        start.setOnClickListener {
+            val drawable = anim_image.drawable
+            (drawable as Animatable).start()
+        }
+    }
+}
